505eb4d055da887eb6fc8bb6c2e023e59315bada
Matt Wilcox <mail@matthewwilcox.com>
2024-07-11 21:04:41 +0100

    Updates

---
+++ _src/_data/site.js
@@ -0,0 +1,9 @@
+export default {
+	"siteTitle": "MattWilcox.net",
+	"copyrightYear": new Date().getFullYear(),
+	"elsewhere": {
+		"mastodon": "https://mstdn.social/@mattwilcox",
+		"github": "https://github.com/MattWilcox",
+		"instagram": "https://instagram.com/MattWilcoxUK"
+	}
+}
