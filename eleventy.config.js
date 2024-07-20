d5700c1cc37e438d7462cd68eed970cfe9847c2c
Matt Wilcox <mail@matthewwilcox.com>
2024-07-20 22:48:17 +0100

    dates are formatted

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,55 +1,65 @@
-//import {
-//	EleventyRenderPlugin,
-//	EleventyI18nPlugin,
-//	EleventyHtmlBasePlugin
-//} from "@11ty/eleventy";
+// import {
+// 	EleventyRenderPlugin,
+// 	EleventyI18nPlugin,
+// 	EleventyHtmlBasePlugin
+// } from "@11ty/eleventy";
 
-const { DateTime } = import("luxon");
+// const { DateTime } = import("luxon");
+import { DateTime } from "luxon";
+
+/* helpers */
+	const nthNumber = (number) => {
+		if (number > 3 && number < 21) return "th";
+		switch (number % 10) {
+			case 1:
+				return "st";
+			case 2:
+				return "nd";
+			case 3:
+				return "rd";
+			default:
+				return "th";
+		}
+	};
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("_src/design-assets");
 
-	/* FILTERS */
-		eleventyConfig.addAsyncFilter(
-			"postDate",
-			async (dateObj, format = "yyyy LLLL dd") => {
-				if (typeof dateObj === "string") {
-					return DateTime.fromISO(dateObj).toFormat(format);
-				} else if (typeof dateObj === "number") {
-					dateObj = new Date(dateObj);
-				}
-				return DateTime.fromJSDate(dateObj).toFormat(format);
+	eleventyConfig.addAsyncFilter(
+		"postDate",
+		async (dateObj) => {
+			let days = DateTime
+				.fromJSDate(dateObj, { zone: 'utc' })
+				.toFormat('dd');
+			let restOfDate = DateTime
+				.fromJSDate(dateObj, { zone: 'utc' })
+				.toFormat('MMMM, yyyy');
+
+			return `${days}<sup>${nthNumber(days)}</sup> ${restOfDate}`;
+		}
+	);
+
+	// Return all the tags used in a collection
+	eleventyConfig.addAsyncFilter(
+		"getAllTags",
+		async collection => {
+			let tagSet = new Set();
+
+			for(let item of collection) {
+				(item.data.tags || []).forEach(tag => tagSet.add(tag));
 			}
-		);
 
-		eleventyConfig.addAsyncFilter(
-			'htmlDateString',
-			async (dateObj) => {
-				return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
-			}
-		);
+			return Array.from(tagSet);
+		}
+	);
 
-		// Return all the tags used in a collection
-		eleventyConfig.addAsyncFilter(
-			"getAllTags",
-			async collection => {
-				let tagSet = new Set();
-
-				for(let item of collection) {
-					(item.data.tags || []).forEach(tag => tagSet.add(tag));
-				}
-
-				return Array.from(tagSet);
-			}
-		);
-
-		eleventyConfig.addAsyncFilter(
-			"filterTagList",
-			async function filterTagList(tags) {
-				return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-			}
-		);
+	eleventyConfig.addAsyncFilter(
+		"filterTagList",
+		async function filterTagList(tags) {
+			return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+		}
+	);
 
 	return {
 		dir: {
